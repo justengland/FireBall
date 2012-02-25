4b6acfe63ca47e48708dea6427d79595f5280be5
Justin England <justengland@gmail.com>
2012-02-25 22:18:47 +0000

    "Second attempt at a checkin"

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
-var express = require('express');
+var http = require('http');
 
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+http.createServer(function (req, res) {
+   res.writeHead(200, {'Content-Type': 'text/plain'});
+   res.end('Hello World\n');
+}).listen(process.env.PORT);
